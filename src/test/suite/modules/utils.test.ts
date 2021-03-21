1372b47e7c75e04a87bb5589c5f6c250da933b2c
a.asadi <ali64b@gmail.com>
2021-03-21 23:48:09 +0330

    add utils.ts tests

---
+++ src/test/suite/modules/utils.test.ts
@@ -0,0 +1,23 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import { jsonToQueryString, uniqBy } from '../../../modules/utils';
+
+
+suite('Utils.ts tests', () => {
+    vscode.window.showInformationMessage('Start all tests.');
+
+    test('uniqBy test', () => {
+        const list: any[] = [
+            { PackageName: "nugetpackagemanagergui", Versions: ["1", "2"] },
+            { PackageName: "SixLaborsCaptcha.Core", Versions: ["1", "2"] },
+        ];
+        var uniqList = uniqBy([...list, { PackageName: "nugetpackagemanagergui", Versions: ["1", "2"] }], "PackageName");
+        assert.deepStrictEqual(list, uniqList);
+    });
+
+    test('jsonToQueryString test', () => {
+        const obj: any = { PackageName: "nugetpackagemanagergui", Versions: "12.22" };
+        var query = jsonToQueryString(obj);
+        assert.deepStrictEqual(query, `?PackageName=nugetpackagemanagergui&Versions=12.22`);
+    });
+});

f9e8f38ad0ce6a6481e5b4924b7e88fdfb5623ab
aliasadidev <ali.asadi.dev@gmail.com>
2022-04-18 20:41:51 +0430

    change default preRelease to true

--- src/modules/nuget.module.ts
+++ src/modules/nuget.module.ts
@@ -14,25 +14,25 @@ import { jsonToQueryString } from './utils';
  * @returns
  */
 function getRequestOptions(authOption: AuthorizationOption, nugetRequestTimeout: number, vscodeHttpConfig: any): RequestOption {
-  const proxyOption = getProxyOption(vscodeHttpConfig);
-  const requestOption: RequestOption = {
-    timeout: nugetRequestTimeout,
-    headers: {}
-  };
+	const proxyOption = getProxyOption(vscodeHttpConfig);
+	const requestOption: RequestOption = {
+		timeout: nugetRequestTimeout,
+		headers: {}
+	};
 
-  if (proxyOption.proxyIsActive) {
-    requestOption.agent = proxyOption.httpsProxyAgent;
-    if (proxyOption.headers)
-      requestOption.headers.push(proxyOption.headers);
-  }
+	if (proxyOption.proxyIsActive) {
+		requestOption.agent = proxyOption.httpsProxyAgent;
+		if (proxyOption.headers)
+			requestOption.headers.push(proxyOption.headers);
+	}
 
-  if (authOption.authType == AuthorizationType[AuthorizationType.basicAuth]) {
-    var bytes = utf8.encode(authOption.username + ":" + authOption.password);
-    var encoded = base64.encode(bytes);
-    requestOption.headers['Authorization'] = 'Basic ' + encoded;
-  }
+	if (authOption.authType == AuthorizationType[AuthorizationType.basicAuth]) {
+		var bytes = utf8.encode(authOption.username + ":" + authOption.password);
+		var encoded = base64.encode(bytes);
+		requestOption.headers['Authorization'] = 'Basic ' + encoded;
+	}
 
-  return requestOption;
+	return requestOption;
 }
 /**
  * Get The package versions
@@ -43,55 +43,56 @@ function getRequestOptions(authOption: AuthorizationOption, nugetRequestTimeout:
  * @returns `PackageVersion`
  */
 async function getPackageVersions(packageName: string, packageSources: PackageSource[], nugetRequestTimeout: number, vscodeHttpConfig: any): Promise<PackageVersion> {
-  let result: PackageVersion | undefined | null;
-  let errors: string[] = [];
-  try {
-    result = await Promise.any(packageSources.map(async (src) => {
-      let url = src.packageVersionsUrl.replace("{{packageName}}", packageName?.toLowerCase());
-      const requestOption = getRequestOptions(src.authorization, nugetRequestTimeout, vscodeHttpConfig);
-      return await fetch(url, requestOption)
-        .then(async response => {
-          const rawResult = await response.text();
-          let jsonResponse;
-          try {
-            jsonResponse = JSON.parse(rawResult);
-            if (jsonResponse && !Array.isArray(jsonResponse.versions)) {
-              throw "not found"
-            }
-          } catch (ex) {
-            errors.push(`
+	let result: PackageVersion | undefined | null;
+	let errors: string[] = [];
+	try {
+		result = await Promise.any(packageSources.map(async (src) => {
+			let url = src.packageVersionsUrl.replace("{{packageName}}", packageName?.toLowerCase());
+			const requestOption = getRequestOptions(src.authorization, nugetRequestTimeout, vscodeHttpConfig);
+			return await fetch(url, requestOption)
+				.then(async response => {
+					const rawResult = await response.text();
+					let jsonResponse;
+					try {
+						jsonResponse = JSON.parse(rawResult);
+						if (jsonResponse && !Array.isArray(jsonResponse.versions)
+							|| (Array.isArray(jsonResponse.versions) && jsonResponse.versions.length == 0)) {
+							throw "not found"
+						}
+					} catch (ex) {
+						errors.push(`
             [NuGet Package Manager GUI => ERROR!!!]
             [Request to url:${url}]
             [timeout:${nugetRequestTimeout}]
             [proxy is active:${!!requestOption.agent}]
             [auth is active:${src.authorization && src.authorization.authType != AuthorizationType[AuthorizationType.none]}]
             [result:${rawResult}]\n`);
-            throw ex;
-          }
+						throw ex;
+					}
 
-          return jsonResponse;
-        })
-        .then(jsonResponse => {
-          let json: PackageVersion = {
-            packageName: packageName,
-            versions: jsonResponse.versions,
-            sourceName: src.sourceName,
-            sourceId: src.id
-          };
-          return json;
-        })
-        .catch(error => {
-          throw `[An error occurred in the loading package versions (package:${packageName})] ${error.message}`;
-        });
-    }));
+					return jsonResponse;
+				})
+				.then(jsonResponse => {
+					let json: PackageVersion = {
+						packageName: packageName,
+						versions: jsonResponse.versions,
+						sourceName: src.sourceName,
+						sourceId: src.id
+					};
+					return json;
+				})
+				.catch(error => {
+					throw `[An error occurred in the loading package versions (package:${packageName})] ${error.message}`;
+				});
+		}));
 
-  } catch (e) {
-    console.log(e);
-    console.log(errors);
-    throw `[An error occurred in the loading package versions (package:${packageName})] details logged in VSCode developer tools`;
-  }
+	} catch (e) {
+		console.log(e);
+		console.log(errors);
+		throw `[An error occurred in the loading package versions (package:${packageName})] details logged in VSCode developer tools`;
+	}
 
-  return <PackageVersion>result;
+	return <PackageVersion>result;
 }
 
 /**
@@ -103,7 +104,7 @@ async function getPackageVersions(packageName: string, packageSources: PackageSo
  * @returns `PackageVersion`
  */
 export async function fetchPackageVersions(packageName: string, packageSources: PackageSource[], nugetRequestTimeout: number, vscodeHttpConfig: any): Promise<PackageVersion> {
-  return getPackageVersions(packageName, packageSources, nugetRequestTimeout, vscodeHttpConfig);
+	return getPackageVersions(packageName, packageSources, nugetRequestTimeout, vscodeHttpConfig);
 }
 
 /**
@@ -116,10 +117,10 @@ export async function fetchPackageVersions(packageName: string, packageSources: 
  */
 export async function fetchPackageVersionsBatch(packages: string[], packageSources: PackageSource[], nugetRequestTimeout: number, vscodeHttpConfig: any): Promise<PackageVersion[]> {
 
-  let result = await Promise.all(
-    packages.map(pkgName => getPackageVersions(pkgName, packageSources, nugetRequestTimeout, vscodeHttpConfig))
-  );
-  return result;
+	let result = await Promise.all(
+		packages.map(pkgName => getPackageVersions(pkgName, packageSources, nugetRequestTimeout, vscodeHttpConfig))
+	);
+	return result;
 }
 /**
  * Search for packages
@@ -134,49 +135,49 @@ export async function fetchPackageVersionsBatch(packages: string[], packageSourc
  */
 export async function searchPackage(query: string, packageSources: PackageSource[], take: number, skip: number, nugetRequestTimeout: number, vscodeHttpConfig: any, packageSourceId?: number): Promise<PackageSearchResult[]> {
 
-  if (packageSourceId != null) {
-    packageSources = packageSources.filter(x => x.id == packageSourceId!)
-  }
+	if (packageSourceId != null) {
+		packageSources = packageSources.filter(x => x.id == packageSourceId!)
+	}
 
-  const results = await Promise.all(
-    packageSources.map(async src => {
-      const queryString = jsonToQueryString({
-        q: query,
-        preRelease: src.preRelease,
-        semVerLevel: "2.0.0",
-        skip: skip,
-        take: take
-      });
-      let url = `${src.searchUrl}${queryString}`;
-      const requestOption = getRequestOptions(src.authorization, nugetRequestTimeout, vscodeHttpConfig);
-      return fetch(url, requestOption)
-        .then(async response => {
-          const rawResult = await response.text();
-          let jsonResponse;
-          try {
-            jsonResponse = JSON.parse(rawResult);
-            jsonResponse.packageSourceName = src.sourceName
-            jsonResponse.packageSourceId = src.id;
-          } catch (ex) {
-            console.log(`[NuGet Package Manager GUI => ERROR!!!]\n[Request to url:${url}]\n[timeout:${requestOption.timeout}]\n[proxy is active:${!!requestOption.agent}]\n[result:${rawResult}]\n`);
-            throw ex;
-          }
+	const results = await Promise.all(
+		packageSources.map(async src => {
+			const queryString = jsonToQueryString({
+				q: query,
+				preRelease: src.preRelease,
+				semVerLevel: "2.0.0",
+				skip: skip,
+				take: take
+			});
+			let url = `${src.searchUrl}${queryString}`;
+			const requestOption = getRequestOptions(src.authorization, nugetRequestTimeout, vscodeHttpConfig);
+			return fetch(url, requestOption)
+				.then(async response => {
+					const rawResult = await response.text();
+					let jsonResponse;
+					try {
+						jsonResponse = JSON.parse(rawResult);
+						jsonResponse.packageSourceName = src.sourceName
+						jsonResponse.packageSourceId = src.id;
+					} catch (ex) {
+						console.log(`[NuGet Package Manager GUI => ERROR!!!]\n[Request to url:${url}]\n[timeout:${requestOption.timeout}]\n[proxy is active:${!!requestOption.agent}]\n[result:${rawResult}]\n`);
+						throw ex;
+					}
 
-          return jsonResponse;
-        })
-        .catch(error => {
-          throw `[An error occurred in the searching package] ${error.message}`;
-        });
-    }));
+					return jsonResponse;
+				})
+				.catch(error => {
+					throw `[An error occurred in the searching package] ${error.message}`;
+				});
+		}));
 
-  return results.map(result => {
-    return ({
-      packageSourceId: result.packageSourceId,
-      packageSourceName: result.packageSourceName,
-      packages: <PackageMetadata[]>result.data ?? [],
-      totalHits: +result.totalHits
-    });
+	return results.map(result => {
+		return ({
+			packageSourceId: result.packageSourceId,
+			packageSourceName: result.packageSourceName,
+			packages: <PackageMetadata[]>result.data ?? [],
+			totalHits: +result.totalHits
+		});
 
-  });
+	});
 
 }
